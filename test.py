19a2ad7e5560eb86e218b77932ea14110d949f7e
Dane Williams <dwilli36@nd.edu>
2020-12-20 14:26:59 -0800

    removes old test files

--- test.py
+++ test.py
@@ -7,12 +7,13 @@ def main():
     s3 = boto3.client('s3')
     print(s3.get_bucket_acl(Bucket='imessage-viz'))
 
-    tructus = {'type': 'image/png', 'name': 'FisherBasementLogo.png'}
+    tructus = {'type': 'application/x-sqlite3', 'name': 'chat.db'}
     response = requests.post('https://68fwpup9dc.execute-api.us-west-1.amazonaws.com/api/', json=tructus)
     guy = response.json()
+    print(guy)
 
-    headers = {'Content-Type': 'image/png'}
-    f = open('./FisherBasementLogo.png', 'rb')
+    headers = {'Content-Type': 'application/x-sqlite3'}
+    f = open('./chat.db', 'rb')
     result = requests.put(guy['body']['url'], headers=headers, data=f)
 
 
